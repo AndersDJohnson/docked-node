9875350c374fcb239a7b16ac50ebcd3716011ccc
Anders D. Johnson <anders.d.johnson.dev@gmail.com>
2019-12-28 12:07:59 -0600

    refactor: docker build iidfile

--- docked-node.sh
+++ docked-node.sh
@@ -4,7 +4,6 @@ DOCKER_NODE_IMAGE="${DOCKER_NODE_IMAGE:-node}"
 
 clean() {
   rm -f "$TMP_DOCKERFILE"
-  rm -f "$TMP_BUILD_OUT"
 }
 
 docker-here() {
@@ -16,11 +15,11 @@ if [ ! -f package.json ]; then
   exit 1
 fi
 
-DOCKER_IMAGE=$(cat .docked-node-image 2>/dev/null || true)
+DOCKER_IMAGE_ID=$(cat .docked-node-image 2>/dev/null || true)
 PKG_SHA_OLD=$(cat .docked-node-package-hash 2>/dev/null || true)
 PKG_SHA_NEW=$(shasum package.json | cut -d' ' -f 1)
 
->&2 echo "Previous docker image: ${DOCKER_IMAGE}"
+>&2 echo "Previous docker image: ${DOCKER_IMAGE_ID}"
 >&2 echo "Previous package hash: ${PKG_SHA_OLD}"
 >&2 echo "Current package hash: ${PKG_SHA_NEW}"
 
@@ -28,8 +27,9 @@ PKG_SHA_NEW=$(shasum package.json | cut -d' ' -f 1)
 
 >&2 echo "Package changes? ${PKG_CHANGE}"
 
-if $PKG_CHANGE || [ -z "$DOCKER_IMAGE" ]; then
-  rm -f "$DOCKER_IMAGE"
+if $PKG_CHANGE || [ -z "$DOCKER_IMAGE_ID" ]; then
+  rm -f .docked-node-image
+  echo "$PKG_SHA_NEW" > .docked-node-package-hash
   >&2 echo "No previous docker image, or package changes. Building docker image..."
   TMP_DOCKERFILE=$(mktemp .Dockerfile.XXXXXX)
   TMP_BUILD_OUT=$(mktemp .docker-build-out.XXXXXX)
@@ -44,17 +44,15 @@ RUN mv node_modules /
 CMD node .
 EOF
   trap clean EXIT
-  docker build --file "$TMP_DOCKERFILE" . > "$TMP_BUILD_OUT"
-  DOCKER_IMAGE=$(tail -n 1 "$TMP_BUILD_OUT" | cut -d' ' -f 3)
-  >&2 echo "Docker image built: ${DOCKER_IMAGE}"
-  echo "$DOCKER_IMAGE" > .docked-node-image
-  echo "$PKG_SHA_NEW" > .docked-node-package-hash
-  >&2 echo "Running node inside docker container ${DOCKER_IMAGE}..."
+  docker build --file "$TMP_DOCKERFILE" --iidfile .docked-node-image . 1>&2
+  DOCKER_IMAGE_ID=$(cat .docked-node-image)
+  >&2 echo "Docker image built: ${DOCKER_IMAGE_ID}"
+  >&2 echo "Running node inside docker container ${DOCKER_IMAGE_ID}..."
   >&2 echo ""
-  docker run --interactive --tty --rm "$DOCKER_IMAGE" "$@"
+  docker run --interactive --tty --rm "$DOCKER_IMAGE_ID" "$@"
 else
   >&2 echo "Docker image already up-to-date."
-  >&2 echo "Running node inside docker container ${DOCKER_IMAGE}..."
+  >&2 echo "Running node inside docker container ${DOCKER_IMAGE_ID}..."
   >&2 echo ""
-  docker-here "$DOCKER_IMAGE" "$@"
+  docker-here "$DOCKER_IMAGE_ID" "$@"
 fi
